52e8c6df32686b03a2a154e99170f329c45a0c98
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-09-01 22:10:55 -0400

    third times the charm

--- src/index.js
+++ src/index.js
@@ -1,108 +1,130 @@
-let addToy = false;
+document.addEventListener("DOMContentLoaded", function() {
 
-const toycollection = document.getElementById("toy-collection")
+  const baseUrl = "http://localhost:3000/toys/"
 
-const getToys = () => {
-  fetch("http://localhost:3000/toys")
-  .then(response => response.json())
-  .then(toyCollection => renderToys(toyCollection))
-}
-// creates const for the function to render the toy object
-const renderToys = toys => {
-  for (const toyObj of toys){
-    (renderToy(toyObj))
+  const getToys = () => {
+    fetch(baseUrl)
+    .then(resp => resp.json())
+    .then(toys => renderToys(toys))
   }
-}
-//create a function to render toy to DOM
-function renderToy(toyObj) {
-  const toyDiv = document.createElement('div')
-  toyDiv.classList.add("card")
-  toyDiv.dataset.id = toyObj.id
-  
-  toyDiv.innerHTML =`
-  <h2>${toyObj.name}</h2>
-  <img src="${toyObj.image}" class="toy-class" width="85%">
-  <p>${toyObj.likes} likes</p>
-  <button class="like-btn">likes</button>
-  
-  `
-  toycollection.appendChild(toyDiv)
-}
 
-function submitData( name, image, likes ) {
-  fetch( 'http://localhost:3000/toys/', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify( {
-        name,
-        image,
-        likes
-      } )
-    } )
-}
-
-function submitLikes(id,likes ) { 
-  fetch( ('http://localhost:3000/toys/'+ id), {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify( { likes: likes
-      } )
-    } )
-}
-
-const submitHandler = () => {
-  document.addEventListener('submit', e => {
-    e.preventDefault()
-    const form = e.target
-    const name = form.name.value
-    const image = form.image.value
-    const likes = 0
-
-    const toyObj = {
-      name: name,
-      image: image,
-      likes: likes
+  const renderToys = (toys) => {
+    for(let toy of toys) {
+      createToy(toy)
     }
+  }
 
-    renderToy(toyObj)
-    submitData(name,image,likes)
-    form.reset()
-  })
-}
+  function createToy(toy){
+    const toyDiv = document.getElementById('toy-collection')
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+    toyDiv.insertAdjacentHTML("beforeend", `
+      <div data-id="${toy.id}" class="card">
+        <h2>${toy.name}</h2>
+        <img src=${toy.image} class="toy-avatar" />
+        <p>${toy.likes} Likes </p>
+        <button data-id="${toy.id}" class="like-btn">Like <3</button>
+        <button data-id="${toy.id}" class="delete-btn">Delete :(</button>
+      </div>
+    `)
+  }
+
+  function clickHandler() {
+    document.addEventListener("click", (e) => {
+      const click = e.target
+      const formContain = document.querySelector('.container')
+
+      if (click.matches('#new-toy-btn')) {
+        if (click.innerText === "Add a new toy!") {
+          formContain.style.display = 'block'
+          click.innerText = "Nevermind"
+        } else if (click.innerText === "Nevermind") {
+          formContain.style.display = 'none'
+          click.innerText = "Add a new toy!"
+        }
+      }
+
+      if (click.matches('.like-btn')) {
+        let likes = click.previousElementSibling
+        let newLikes = parseInt(likes.innerText.split(" ")[0]) + 1
+        likes.innerText = newLikes + " Likes"
+
+        const id = click.dataset.id
+
+        fetch(baseUrl + id, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            "accept": "application/json"
+          },
+          body: JSON.stringify({
+            likes: newLikes + " Likes"
+          })
+        })
+      }
+
+      if (click.matches('.delete-btn')) {
+        let id = click.dataset.id
+
+        fetch(baseUrl + id, {
+          method: "DELETE"
+        })
+        .then(resp => resp.json())
+        .then(
+          click.parentElement.remove()
+        )
+      }
+    })
+  }
+
+  function submitHandler() {
+    document.addEventListener("submit", function(e) {
+      e.preventDefault()
+
+      let form = document.querySelector('.add-toy-form')
+      let name = form.name.value
+      let image = form.image.value
+
+      form.reset()
+
+      fetch(baseUrl, { 
+        method: "POST",
+        headers: { 
+          "Content-type": "application/json",
+          "accept": "application/json"
+        },
+        body: JSON.stringify({
+          "name": name,
+          "image": image,
+          "likes": 0
+        })
+      })
+      .then(resp => resp.json())
+      .then(data => {
+        createToy(data)
+      })
+    })
+
+
+  }
+
   getToys()
-  
-  document.addEventListener('click', function(e){
-    if (e.target.innerText === 'likes') {
-      newNum = parseInt(e.target.parentElement.querySelector('p').innerText, 10)+1
-      //can also use previousElementSilbing
-      idNum = e.target.parentElement.dataset.id
-      e.target.parentElement.querySelector('p').innerHTML = `${newNum} likes`
-      submitLikes(idNum, newNum)
-    }     
-  })
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    // I am very bad with typing
-    console.log("you are at the top level")
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-      addBtn.innerHTML="Cancel" 
-      submitHandler()
-    } else {
-      toyFormContainer.style.display = "none";
-      addBtn.innerHTML="Add a new toy!"
-    }
-  });
-});
+  clickHandler()
+  submitHandler()
+})
 
+
+/*
+http://localhost:3000/toys
+1) send GET request and render TOYS on DOM
+2) add to toy-collection div and each toy has DIV
+  <div class="card">
+    <h2>Woody</h2>
+    <img src=toy_image_url class="toy-avatar" />
+    <p>4 Likes </p>
+    <button class="like-btn">Like <3</button>
+  </div>
+  3) when new toy is added, should show to the page by POST
+  4) when like button is pressed, update likes by sending PATCH to
+  `http://localhost:3000/toys/:id`
+  5) when delete button is pressed, delete toy by sending DELETE
+ */
